ac86a611624e3eaf66159c71c2a2747a7dbfc8c8
Marcel Mokos <marcel.mokos@gmail.com>
2021-04-02 12:25:11 +0200

    add default for api url without .env

--- src/service/axiosInstance.tsx
+++ src/service/axiosInstance.tsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const apiUrl = process.env.REACT_APP_API_URL;
+export const apiUrl =
+  process.env.REACT_APP_API_URL ??
+  "https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343";
 
 const instance = axios.create({
   baseURL: apiUrl,
